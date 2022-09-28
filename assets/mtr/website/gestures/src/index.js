e7be2400c82af8835deecce1d70c37e045df6079
X-Funny <1150869019@qq.com>
2022-09-28 23:59:23 +0800

    alpha0.5.2-1

--- assets/mtr/website/gestures/src/index.js
+++
@@ -1,9 +0,0 @@
-import pinchable from './gestures/pinch'
-import panable from './gestures/pan'
-import tappable from './gestures/tap'
-
-export default {
-  pinchable,
-  panable,
-  tappable
-}
--- assets/mtr/website/version.js
+++
@@ -1,3 +0,0 @@
-const VERSION = "1.18.2-3.1.0-prerelease-1"
-
-export default VERSION;
